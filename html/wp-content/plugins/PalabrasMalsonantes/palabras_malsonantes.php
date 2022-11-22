036054894423fa93e0c0ec2dd4c58eae7217b60c
EricBorder <magicsergy@gmail.com>
2022-11-22 13:03:42 +0100

    Arreglo plugin para que funcione en Wordpress y sustituya las palabras malsonantes por las palabras aceptables

--- html/wp-content/plugins/PalabrasMalsonantes/palabras_malsonantes.php
+++ html/wp-content/plugins/PalabrasMalsonantes/palabras_malsonantes.php
@@ -1,15 +1,16 @@
 <?php
 /*
 Plugin Name: Palabras Malsonantes
-Plugin URI: 
+Plugin URI:
 Description: Busca palabras malsonantes y las cambia por sinónimos más aceptables
 Author. Sergio Fernández
 Version: 1.0
-Author URI: 
+Author URI:
 */
 
 
-function crearTablas() {
+function crearTablas()
+{
     global $wpdb;
     $charset_collate = $wpdb->get_charset_collate();
 
@@ -18,64 +19,88 @@ function crearTablas() {
 
     $sql = "CREATE TABLE $table_name1 (
         id mediumint(9) NOT NULL,
-        text text NOT NULL,
+        palabra text NOT NULL,
         PRIMARY KEY (id)
     ) $charset_collate;";
 
     $sql2 = "CREATE TABLE $table_name2 (
         id mediumint(9) NOT NULL,
-        text text NOT NULL,
+        palabra text NOT NULL,
         PRIMARY KEY (id)
     ) $charset_collate;";
 
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
-    dbDelta( $sql2 );
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+    dbDelta($sql2);
 }
+
+
 add_action('plugins_loaded', 'crearTablas');
 
 
-function insertValoresTablas() {
+function insertValoresTablas()
+{
     global $wpdb;
     $table_name1 = $wpdb->prefix . 'palabras_malsonantes';
     $table_name2 = $wpdb->prefix . 'palabras_reemplazo';
 
-    $sql11 = "INSERT INTO $table_name1 (id, text) VALUES (1, 'caca')";
-    $sql12 = "INSERT INTO $table_name1 (id, text) VALUES (2, 'culo')";
-    $sql13 = "INSERT INTO $table_name1 (id, text) VALUES (3, 'pedo')";
-    $sql14 = "INSERT INTO $table_name1 (id, text) VALUES (4, 'pis')";
-    $sql15 = "INSERT INTO $table_name1 (id, text) VALUES (5, 'pirola')";
+    $sql11 = "INSERT INTO $table_name1 (id, palabra) VALUES (1, 'caca')";
+    $sql12 = "INSERT INTO $table_name1 (id, palabra) VALUES (2, 'culo')";
+    $sql13 = "INSERT INTO $table_name1 (id, palabra) VALUES (3, 'pedo')";
+    $sql14 = "INSERT INTO $table_name1 (id, palabra) VALUES (4, 'pis')";
+    $sql15 = "INSERT INTO $table_name1 (id, palabra) VALUES (5, 'pirola')";
 
-    $sql21 = "INSERT INTO $table_name2 (id, text) VALUES (1, 'excremento')";
-    $sql22 = "INSERT INTO $table_name2 (id, text) VALUES (2, 'trasero')";
-    $sql23 = "INSERT INTO $table_name2 (id, text) VALUES (3, 'flatulencia')";
-    $sql24 = "INSERT INTO $table_name2 (id, text) VALUES (4, 'orina')";
-    $sql25 = "INSERT INTO $table_name2 (id, text) VALUES (5, 'pene')";
+    $sql21 = "INSERT INTO $table_name2 (id, palabra) VALUES (1, 'excremento')";
+    $sql22 = "INSERT INTO $table_name2 (id, palabra) VALUES (2, 'trasero')";
+    $sql23 = "INSERT INTO $table_name2 (id, palabra) VALUES (3, 'flatulencia')";
+    $sql24 = "INSERT INTO $table_name2 (id, palabra) VALUES (4, 'orina')";
+    $sql25 = "INSERT INTO $table_name2 (id, palabra) VALUES (5, 'pene')";
 
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql11);
-    dbDelta( $sql12);
-    dbDelta( $sql13);
-    dbDelta( $sql14);
-    dbDelta( $sql15);
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql11);
+    dbDelta($sql12);
+    dbDelta($sql13);
+    dbDelta($sql14);
+    dbDelta($sql15);
 
-    dbDelta( $sql21);
-    dbDelta( $sql22);
-    dbDelta( $sql23);
-    dbDelta( $sql24);
-    dbDelta( $sql25);
+    dbDelta($sql21);
+    dbDelta($sql22);
+    dbDelta($sql23);
+    dbDelta($sql24);
+    dbDelta($sql25);
 }
+
 add_action('plugins_loaded', 'insertValoresTablas');
 
 
-function reescribir_malsonantes( $text ) {
+function reescribir_malsonantes($text){
+
     global $wpdb;
     $table_malsonantes = $wpdb->prefix . 'palabras_malsonantes';
-    $table_reemplazo = $wpdb->prefix . 'palabras_malsonantes';
+    $table_reemplazo = $wpdb->prefix . 'palabras_reemplazo';
 
-    $malsonantes = dbDelta("SELECT text FROM $table_malsonantes");
-    $reemplazos = dbDelta("SELECT text FROM $table_reemplazo");
+    $queryMalsonantes = $wpdb->get_results("SELECT palabra FROM $table_malsonantes",ARRAY_A);
+    $queryReemplazos = $wpdb->get_results("SELECT palabra FROM $table_reemplazo",ARRAY_A);
 
-    return str_replace( $malsonantes, $reemplazos, $text );
+    $malsonantes = array();
+    /*for ($i = 0; $i < sizeof($queryMalsonantes); $i++) {
+        $malsonantes[] = $queryMalsonantes[$i]['text'];
+    }*/
+    foreach ($queryMalsonantes as $malsonante) {
+        $malsonantes[] = $malsonante['palabra'];
+    }
+
+    $reemplazos = array();
+    /*for ($i = 0; $i < sizeof($queryReemplazos); $i++) {
+        $reemplazos[] = $queryReemplazos[$i]['text'];
+    }*/
+    foreach ($queryReemplazos as $reemplazo) {
+        $reemplazos[] = $reemplazo['palabra'];
+    }
+
+
+    return str_replace($malsonantes, $reemplazos, $text);
+
 }
-add_filter( 'the_content', 'reescribir_malsonantes' );
+
+add_filter('the_content', 'reescribir_malsonantes');
